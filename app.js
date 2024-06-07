aae0d18c1b20984fe58ac1a60ac569627d987938
Gabriel Tavares <gsantostavares@gmail.com>
2024-06-07 19:53:38 -0300

    Versão que gerou os pdf

--- app.js
+++ app.js
@@ -1,57 +1,11 @@
+let csvToJson = require('convert-csv-to-json')
 const htmlPdf = require('html-pdf');
+QRCode = require('qrcode')
+const fs = require('fs')
+
+let json = csvToJson.getJsonFromCsv("beneficiamento.csv")
 
 
-barcd = ''
-triId = ''
-lincd = ''
-prjCd = ''
-familia = ''
-
-arquivo =
-    `<!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Qr Code - UBS</title>
-        <style>
-        td {
-            font-size: 30px;
-        }
-    </style>
-    </head>
-    <body>
-        
-        <div>
-         ${barcd}   
-        </div>
-        <div>
-            <table>
-                <tr>
-                    <td>TrlID:</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>LocName:</td>
-                    <td>BR PLM Romaria MG</td>
-                </tr>
-                <tr>
-                    <td>LINCD:</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>PrjCD:</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Familia:</td>
-                    <td>${familia}</td>
-                </tr>
-            </table>
-        </div>
-    </body>
-    </html>
-    `
 
 function convertHtmlToPdf(htmlContent, outputFilePath) {
     const options = { format: 'A4' };
@@ -65,5 +19,70 @@ function convertHtmlToPdf(htmlContent, outputFilePath) {
     });
 }
 
-const outputFilePath = 'qrcode.pdf';
-convertHtmlToPdf(arquivo, outputFilePath);
+for (let i = 0; i < json.length; i++) {
+
+    barcd = json[i]['BARCD']
+    triId = json[i]['TriID(MATID)']
+    lincd = json[i]['LINCD']
+    prjCd = json[i]['PrjCD']
+    familia = json[i]['Familia']
+    imagem = `qrcode/${barcd}.png`
+
+    const imageBuffer = fs.readFileSync(imagem);
+    const base64Image = imageBuffer.toString('base64');
+
+    arquivo =
+        `<!DOCTYPE html>
+        <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Qr Code - UBS</title>
+                        <style>
+                            td {
+                                font-size: 50px;
+                                }
+                        </style>
+                    </head>
+                    <body>
+                        <div>
+                            <img src="data:image/png;base64,${base64Image}" alt="">
+                        </div>
+                        
+                        <div>
+                            <table>
+                                <tr>
+                                    <td>TrlID:</td>
+                                    <td>${triId}</td>
+                                </tr>
+                                <tr>
+                                    <td>LocName:</td>
+                                    <td>BR PLM Romaria MG</td>
+                                </tr>
+                                <tr>
+                                    <td>LINCD:</td>
+                                    <td>${lincd}</td>
+                                </tr>
+                                <tr>
+                                    <td>PrjCD:</td>
+                                    <td>${prjCd}</td>
+                                </tr>
+                                <tr>
+                                    <td>Familia:</td>
+                                    <td>${familia}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </body>
+                </html>
+            `
+
+    const outputFilePath = `${barcd}.pdf`;
+    convertHtmlToPdf(arquivo, outputFilePath);
+
+
+}
+
+
+
+
